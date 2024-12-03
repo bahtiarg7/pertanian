5f456dfa66c44c4f09f1ebefde62680e2ea9cd4d
nandaauliaramadhan <bahtiarg7@gmail.com>
2024-12-03 10:49:08 +0700

    update

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -27,7 +27,8 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/dashboard';
+    // protected $redirectTo = '/dashboard';
+    protected $redirectTo = '/backend/dashboard';
 
     /**
      * Create a new controller instance.
